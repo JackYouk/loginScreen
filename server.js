a90466891d11d18dc9a08e58edb34cd8e9400388
JackYouk <jackyoukstetter1@gmail.com>
2022-07-16 23:49:12 -0700

    built server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+const routes = require('./routes');
+
+// Model
+const Login = require('./model/Login');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(routes);
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => console.log(`App running on PORT ${PORT}`));
+});
