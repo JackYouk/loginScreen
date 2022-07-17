a4cf6261f7ae1076d8b3a3adb946bebe126b8f1a
JackYouk <jackyoukstetter1@gmail.com>
2022-07-17 00:19:15 -0700

    fixed html file, added route to send user the html file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./apiRoutes');
-const ui = require('../public');
+const path = require('path');
 
 router.use('/api', apiRoutes);
 
-router.use('/', ui);
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
 
 module.exports = router;
